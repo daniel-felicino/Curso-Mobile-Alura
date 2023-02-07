615db6fbac59d7e445dfd9556397ae58b6d38b5b
https://github.com/daniel-felicino <daniel-feliciano@hotmail.com>
2023-02-07 13:15:15 -0300

    atualização

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ menu.addEventListener('click', function () {
     var listaMenu = document.getElementById('listaMenu')
     listaMenu.classList.toggle('aparecerMenu')
 
-}) 
+})
+
+var categorias = document.getElementById('categorias');
+categorias.addEventListener('click', () => {
+
+    var listaMenu = document.getElementById('listaMenu2')
+    listaMenu.classList.toggle('aparecerMenu2')
+})
